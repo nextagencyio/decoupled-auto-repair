cb1023aa3a29e756d5270a523a0305e94c55d074
Jay Callicott <jrcallicott@gmail.com>
2026-03-08 08:37:47 -0500

    feat: add Cloudflare Pages compatibility

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const path = require('path')
 const nextConfig = {
   outputFileTracingRoot: path.resolve(__dirname),
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'http',
